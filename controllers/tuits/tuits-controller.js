2ce9e1f1bff5e19de42edeb154fda3336eec56c3
Ryan <ryanlooney58@gmail.com>
2022-11-16 12:22:43 -0500

    updating create tuit code

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -9,9 +9,9 @@ export default (app) => {
 
 const createTuit = async (req, res) => {
     const newTuit = req.body;
-    newTuit.topic = "tuiter"
-    newTuit.userName = "Ryan Looney"
-    newTuit.time = "now"
+    newTuit.topic = "tuiter";
+    newTuit.userName = "Ryan Looney";
+    newTuit.time = "now";
     newTuit.userImage = "../images/profile_images/tuiter_profile_picture.jpg";
     newTuit.liked = false;
     newTuit.disliked = false;
@@ -20,6 +20,7 @@ const createTuit = async (req, res) => {
     newTuit.likes = 0;
     newTuit.dislikes = 0;
     newTuit.userHandle = "@rloon";
+    console.log(newTuit);
     const insertedTuit = await tuitsDAO.createTuit(newTuit);
     res.json(insertedTuit);
 }
